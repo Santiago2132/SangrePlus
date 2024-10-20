41d64491d4c1fe8a2c6b4f151e41b3dec09877b1
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2024-10-19 23:10:17 -0500

    Ya nada funciona

--- Cliente/src/index/Index.ts
+++ Cliente/src/index/Index.ts
@@ -1,6 +1,4 @@
 import IndexController from './controller/IndexController.js';
-import IndexTemplate from './template/IndexTemplate.js';
-import IndexView from './view/IndexView.js';
 
 export default class Index {
     public static create =(): IndexController => {   
