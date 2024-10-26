433236e0fe30419679046cf80d60653bcb9354e5
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2024-10-25 23:27:20 -0500

    No se por que no se renderiza el admin pero X

--- Cliente/src/index.ts
+++ Cliente/src/index.ts
@@ -1,5 +1,4 @@
 
 import menu from "./menu/menu.js";
 // Ejemplo de cómo usar la clase Index para inicializar la aplicación
-
 menu.create()
--- Cliente/src/menu/menu.ts
+++ Cliente/src/menu/menu.ts
@@ -9,7 +9,7 @@ export default class menu { constructor(){
         controller.init()
         return controller
 
-     }
+    }
     
 
 
